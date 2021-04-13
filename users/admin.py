ce7433eb3591fbcbf36054710b061cf702ef0484
hanum <rntls123@naver.com>
2021-04-13 21:51:51 +0900

    4.13 21:51 Create custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+# admin.site.register(models.User, CustomUserAdmin)  이렇게 할 수도 있음
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    # class CustomUserAdmin(admin.ModelAdmin):
+    # 이렇게 하는 방법도 있음. 그러나 장고에서 제공해주는 userAdmin 전용 패널을 사용할 것임.
+
+    """ Custom User Admin """
+
+    # list_display = (
+    #     "username",
+    #     "gender",
+    #     "language",
+    #     "currency",
+    #     "superhost",
+    # )  user list 에서 항목을 보여주는 옵션
+
+    # list_filter = (
+    #     "superhost",
+    #     "language",
+    #  filter 를 만드는 옵션
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                ),
+            },
+        ),
+    )
--- users/models.py
+++ users/models.py
@@ -1,4 +1,4 @@
-# from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
@@ -6,4 +6,38 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
 
-    pass
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),  # GENDER_MALE 이 admin 패널로 전송, Male 이 Form 에 보여진다.
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "ko"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_ENGLISH, "English"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_KRW, "KRW"), (CURRENCY_USD, "USD"))
+
+    avatar = models.ImageField(blank=True)  # 데이터베이스의 값이 null 이어도 허용
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, blank=True
+    )  # blank 이용 필수입력항목 해제
+    bio = models.TextField(blank=True)  # 데이터베이스 기본값을 "" 로 설정
+    # CharField 는 한줄, 글자수 제한. TextField 는 자유.
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
