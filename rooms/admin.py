8be5fa719411101a07ef05805343315c67d8ede2
hanum <rntls123@naver.com>
2021-04-16 19:54:24 +0900

    04.16 19:54 Setting More Room Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -9,7 +9,10 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
-    pass
+    list_display = ("name", "used_by")
+
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -17,7 +20,84 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("host", "name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        (
+            "Spaces",
+            {
+                "fields": (
+                    "guests",
+                    "beds",
+                    "bedrooms",
+                    "baths",
+                )
+            },
+        ),
+        (
+            "More About the Space",
+            {
+                "fields": (
+                    "room_type",
+                    "amenities",
+                    "facilities",
+                    "house_rules",
+                )
+            },
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",  # admin function (아래)
+        "count_photos",
+    )
+
+    # ordering = ("price",)  # 패널이 로딩되면 바로 무엇을 기준으로 sort 할 것인지 선택.
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",  # admin 에서 Foreign Key 접근 방식 __
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "host__username")
+    # admin 패널 Room 에서 city 명으로 검색하는 seacrh bar 생성.
+
+    filter_horizontal = (  # ManytoMany Field 에서 수평으로 Filter 사용.
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    # Admin function
+    def count_amenities(self, obj):  # self: RoomAdmin 클래스, obj: 패널의 행 (row).
+
+        return obj.amenities.count()  # row 의 amenities 로 접근, 개수를 확인.
+
+    # count_amenities.short_description = "abc"  # 패널 열 (column) 명 지정
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
--- rooms/models.py
+++ rooms/models.py
@@ -60,7 +60,7 @@ class Photo(core_models.TimeStampedModel):
 
     caption = models.CharField(max_length=80)
     file = models.ImageField()
-    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+    room = models.ForeignKey("Room", related_name="photos", on_delete=models.CASCADE)
     # "Room" 처리한 이유는 Room 클래스가 아래에 있기 떄문에 Photo 를 아래로 옮기거나 string 처리해서 읽을 수 있음. 장고의 기능.
 
     def __str__(self):
@@ -84,13 +84,17 @@ class Room(core_models.TimeStampedModel):
     check_in = models.TimeField()
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
-    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    host = models.ForeignKey(
+        "users.User", related_name="rooms", on_delete=models.CASCADE
+    )
     # foreignKey 는 many to one 구조. 방은 host 를 한 명 밖에 가질 수 없지만 host 는 여러 room 을 가질 수 있음.
-    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    room_type = models.ForeignKey(
+        "RoomType", related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
     # many to many 구조를 사용해야할 때는 ManyToMajyField 를 사용한다.
-    amenities = models.ManyToManyField("Amenity", blank=True)
-    facilities = models.ManyToManyField("Facility", blank=True)
-    house_rules = models.ManyToManyField("HouseRule", blank=True)
+    amenities = models.ManyToManyField("Amenity", related_name="rooms", blank=True)
+    facilities = models.ManyToManyField("Facility", related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
