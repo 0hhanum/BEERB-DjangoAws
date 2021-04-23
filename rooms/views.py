42ff6f1dc4579876df81c2be0a6d8e6cb7174f09
hanum <rntls123@naver.com>
2021-04-23 20:01:31 +0900

    04.23 20:00 Making Class Based View

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+# from django.shortcuts import render, redirect
+# from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 # 수동으로 pagination 만들기
@@ -22,12 +24,28 @@ from . import models
 #         },
 #     )
 
+# Function Based View FBV
+# def all_rooms(request):
+#     page = request.GET.get("page", 1)
+#     room_list = models.Room.objects.all()
+#     # 지금이 아닌 room_list 가 호출됐을 때 query set 을 불러온다. DataBase 가 방대할 때는 치명적일 수 있으니 조심.
+#     paginator = Paginator(room_list, 10, orphans=5)  # 자투리 다섯개까지는 마지막 페이지로 밀어넣기
+#     # rooms = paginator.get_page(page)  # 10 단위로 쪼갰을 때 get 으로 얻은 page 에 해당하는 page 객체.
+#     try:
+#         rooms = paginator.page(int(page))
 
-def all_rooms(request):
-    page = int(request.GET.get("page", 1))
-    room_list = models.Room.objects.all()
-    # 지금이 아닌 room_list 가 호출됐을 때 query set 을 불러온다. DataBase 가 방대할 때는 치명적일 수 있으니 조심.
-    paginator = Paginator(room_list, 10, orphans=5)  # 자투리 다섯개까지는 마지막 페이지로 밀어넣기
-    # rooms = paginator.get_page(page)  # 10 단위로 쪼갰을 때 get 으로 얻은 page 에 해당하는 page 객체.
-    rooms = paginator.page(page)
-    return render(request, "rooms/home.html", context={"page": rooms})
+#         return render(request, "rooms/home.html", context={"page": rooms})
+
+#     except EmptyPage:
+#         return redirect("/")
+
+
+class HomeView(ListView):  # Class Based View
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
