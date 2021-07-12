24b53f31ca97b63faf36d0f878d823736150a95b
hanum <rntls123@naver.com>
2021-07-12 22:12:01 +0900

    #71

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -51,7 +51,7 @@ class Command(BaseCommand):
                 room_models.Photo.objects.create(
                     caption=seeder.faker.sentence(),
                     room=room,
-                    file=f"room-photos/{random.randint(1,31).webp}",
+                    file=f"room-photos/{random.randint(1,31)}.webp",
                 )
 
             for facility in facilities:
