ccfb749a74746240396d67f4a72c22f1aa4570ee
hanum <rntls123@naver.com>
2021-07-12 22:01:56 +0900

    #71

--- users/management/commands/createsu.py
+++ users/management/commands/createsu.py
@@ -1,3 +1,4 @@
+import os
 from django.core.management.base import BaseCommand
 from users.models import User
 
@@ -7,8 +8,14 @@ class Command(BaseCommand):
     help = "This command create superuser"
 
     def handle(self, *args, **options):
+        admin_id = os.environ.get("admin")
+        admin_password = os.environ.get("ad_password")
 
-        admin = User.objects.get_or_none(username="ebadmin")
+        admin = User.objects.get_or_none(username=admin_id)
         if not admin:
-            User.objects.create_superuser("ebadmin", "rntls123@naver.com", "123")
+            User.objects.create_superuser(
+                admin_id,
+                "rntls123@naver.com",
+                admin_password,
+            )
             self.stdout.write(self.style.SUCCESS("Supersuer Created"))
